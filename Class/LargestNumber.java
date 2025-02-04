7a037e4bb1b0a89d08bd61b4144f770d3ab27ebf
Harshika Gupta <harshikagupta098@gmail.com>
2025-02-04 13:27:20 +0530

    largest number code

---
+++ Class/LargestNumber.java
@@ -0,0 +1,27 @@
+//Write a java program to check largest of the 5 input numbers( using integer)
+import java.util.*;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest number is: " + max);
+    }
+
+    'public static void main(String() args){
+    
+    }
+    
+}
